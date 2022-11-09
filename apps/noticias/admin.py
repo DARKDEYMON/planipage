100c7612da6d52e1b071deeb6ce23c7e0503bf04
Reynaldo Juan Pereira Heredia <darkdeymon04@gmail.com>
2022-11-09 12:45:55 -0400

    imagenes

--- apps/noticias/admin.py
+++ apps/noticias/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-
+admin.site.register(Pagina)
+admin.site.register(Autoridades)
+admin.site.register(Departamento)
+admin.site.register(Tipo)
 admin.site.register(Publicacion)
 admin.site.register(Archivo)
-admin.site.register(Pagina)
